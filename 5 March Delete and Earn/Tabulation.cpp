914fa05c0ba78922957b5847981218eaf49a794b
Palak Mittal <66310688+palak2104@users.noreply.github.com>
2022-03-05 17:26:51 +0530

    Update Tabulation.cpp

--- 5 March Delete and Earn/Tabulation.cpp
+++ 5 March Delete and Earn/Tabulation.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& arr) {
-      vector<int>freq(10001);
-      vector<int>dp(10001);
+      int mini=INT_MAX;
+      int maxi=INT_MIN;
+      for(auto it:arr){
+          mini=min(mini,it);
+          maxi=max(maxi,it);
+      }
+      vector<int>freq(maxi+1);
+      vector<int>dp(maxi+1);
       for(auto it:arr)
       freq[it]++;
       dp[1]=freq[1];
-      for(int i=2;i<=10000;i++){
+      for(int i=2;i<=maxi;i++){
           dp[i]=max(dp[i-1],i*freq[i]+dp[i-2]);
       }
-      return dp[10000];
+      return dp[maxi];
     }
 };
